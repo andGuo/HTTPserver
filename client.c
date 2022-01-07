a8ff0c01d8bf6eb391bb00cd3eb39aea5e751118
Andrew Guo <aguo@Andrews-MacBook-Pro.local>
2022-01-07 17:02:36 -0500

    Implemented client main

--- client.c
+++ client.c
@@ -3,24 +3,8 @@
 int main(int argc, char const *argv[])
 {
     int serverFd;
-    struct sockaddr_in serverAddress;
-    int addressLen = sizeof(serverAddress);
-
-    if ((serverFd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("Unable to create socket");
-        return -1;
-    }
-
-    memset((char *)&serverAddress, 0, addressLen);
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(PORT);
-
-    if (connect(serverFd, (struct sockaddr*)&serverAddress, addressLen) < 0)
-    {
-        perror("Unable to connect to server");
-        return -1;
-    }
+    
+    connectClient(&serverFd);
 
     doClientRequest(serverFd);
 
