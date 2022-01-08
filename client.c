9fa9aea5c32d1d20cdd1d0e3fd62c844e14a03d1
Andrew Guo <aguo@Andrews-MacBook-Pro.local>
2022-01-08 17:01:08 -0500

    Updated server and client handling functions

--- client.c
+++ client.c
@@ -16,7 +16,7 @@ void doClientRequest(int serverFd)
     long valread;
     char buffer[MAX_BUFFER_SIZE] = {0};
 
-    send(serverFd, "Here's your message\n", strlen("Here's your message\n"), 0);
+    errorCheck(send(serverFd, "Here's your message\n", strlen("Here's your message\n"), 0), "Unable to send data");
     valread = read(serverFd, buffer, MAX_BUFFER_SIZE);
 
     printf("%s\n", buffer);
--- server.c
+++ server.c
@@ -20,10 +20,9 @@ int main(int argc, char *argv[])
 void serveOneClient(int clientFd)
 {
     char buffer[MAX_BUFFER_SIZE] = {0};
-    long valread;
     char *hello = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\nHello world!";
 
-    valread = read(clientFd, buffer, MAX_BUFFER_SIZE);
+    errorCheck(recv(clientFd, buffer, MAX_BUFFER_SIZE, 0), "Unable to receive data");
     printf("%s", buffer);
     printf("Sending reply...\n");
     write(clientFd, hello, strlen(hello));
